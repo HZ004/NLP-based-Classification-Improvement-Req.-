d00bb67a6ea8c13841dacbd1f1ff1796e2c4c307
HZ004 <97055360+HZ004@users.noreply.github.com>
2023-01-02 22:24:25 +0530

    Update project2_deployment.py

--- project2_deployment.py
+++ project2_deployment.py
@@ -135,5 +135,16 @@ best_model = keras.models.load_model("best_model1.hdf5")
 
 predictions = best_model.predict(X_test)
 
-st.header('The predicted sentiment is: ',np.argmax(predictions))
+st.header('The predicted sentiment is: ')
+#np.argmax(predictions))
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:50px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown('<p class="big-font">np.argmax(predictions))</p>', unsafe_allow_html=True)
 
